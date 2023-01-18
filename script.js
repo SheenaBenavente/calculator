073bb387a01f6912080893cfc96f1d5ff313801f
sheena benavente <13785060+SheenaBenavente@users.noreply.github>
2023-01-18 14:39:20 -0500

    updated script for operators and operands to work together.

--- script.js
+++ script.js
@@ -1,37 +1,111 @@
 let operator = '';
 let previousValue = '';
-let currentValue = '';
+let calculatedValue = 0;
 
-document.addEventListener("DOMContentLoaded", function(){
-    //Store all components on HTML in our JS
-    let clear = document.querySelector("#clear");
-    let decimal = document.querySelector(".decimal");
-    let equal = document.querySelector(".equal");
+let currentDisplay = ''; // This var will be used to track what's shown on screen
+let leftValue = '';
+let rightValue = '';
+let clearBtn;
+let decimalBtn;
+let equalBtn;
 
-    let operators = document.querySelectorAll(".operator");
-    let numbers = document.querySelectorAll(".number");
+let operatorBtns;
+let numberBtns;
 
-    let previousScreen = document.querySelector(".previous");
-    let currentScreen = document.querySelector(".current");
+let previousScreenContainer;
+let currentScreenContainer;
 
-    //Grabs the numbers from the DOM when element is clicked
-    numbers.forEach((number) => number.addEventListener("click", function(e){
-      handleNumber(e.target.textContent)
-      currentScreen.textContent = currentValue;
-    }))
-})
+
+document.addEventListener("DOMContentLoaded", function() {
+  //Store all components on HTML in our JS
+  clearBtn = document.querySelector("#clear");
+  decimalBtn = document.querySelector(".decimal");
+  equalBtn = document.querySelector(".equal");
+
+  operatorBtns = document.querySelectorAll(".operator");
+  numberBtns = document.querySelectorAll(".number");
+
+  previousScreenContainer = document.querySelector(".previous");
+  currentScreenContainer = document.querySelector(".current");
+
+  equalBtn.addEventListener('click', function() {
+    calculatedValue = operate();
+    console.log(calculatedValue);
+    previousScreenContainer.innerText = calculatedValue;
+    currentScreenContainer.textContent = '';
+  });
+
+  //Grabs the numbers from the DOM when element is clicked
+  numberBtns.forEach((number) => number.addEventListener("click", function(e) {
+    // handleNumber(e.target.textContent)
+    currentDisplay += e.target.textContent;
+    currentScreenContainer.textContent = currentDisplay;
+
+    if (operator === '') {
+      leftValue += e.target.textContent;
+    } else {
+      rightValue += e.target.textContent;
+    }
+  }));
+
+  operatorBtns.forEach((operatorBtn) => operatorBtn.addEventListener("click", function(e) {
+    operator = e.target.textContent;
+    console.log(operator);
+    currentDisplay = currentDisplay + operator;
+    currentScreenContainer.textContent = currentDisplay;
+  }));
+
+  clearBtn.addEventListener('click', clearInputs);
+});
+/**
+1.load html content, css
+2.clear input
+3.store number button pressed
+4.store operator button pressed
+5. caluculate operation
+6. update result
+7. clear result when C is pressed
+*/
+
+function clearInputs() {
+  calculatedValue = 0;
+  currentDisplay = '';
+  leftValue = '';
+  rightValue = '';
+  operator = '';
+  currentScreenContainer.textContent = '';
+  previousScreenContainer.textContent = '';
+}
+
 
 //Function limits length to 9 digits and displays current value on screen
-function handleNumber(num){
-  if(currentValue.length <= 9){
+// '1111111'
+function handleNumber(num) {
+  console.log(currentValue.toString().length, currentValue)
+  if (currentValue.toString().length < 9) {
     currentValue += num;
   }
 }
 
-console.log("hi")
-console.log("bye")
+function operate() {
+  console.log(leftValue, rightValue, operator);
+  // Call the appropriate function based on the operator
+  const number1 = parseInt(leftValue);
+  const number2 = parseInt(rightValue);
+  let result;
 
+  if (operator === '+') {
+    result = add(number1, number2);
+  } else if (operator === '-') {
+    result = subtract(number1, number2);
+  } else if (operator === 'x') {
+    result = multiply(number1, number2);
+  } else if (operator === '/') {
+    result = divide(number1, number2);
+  }
 
+  return result;
+}
 
 //Read inputs from the user
 //const num1 = prompt("Enter a number: ");
@@ -43,30 +117,20 @@ console.log("bye")
 //const number2 = Number(num2);
 
 // Define the functions for the different operations
-// function add(a, b) {
-//     return a + b;
-// }
-// function subtract(a, b) {
-//     return a - b;
-// }
-// function multiply(a, b) {
-//     return a * b;
-// }
-// function divide(a, b) {
-//     return a / b;
-// }
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
 
-// // Call the appropriate function based on the operator
-// let result;
-// if (operator === '+') {
-//   result = add(number1, number2);
-// } else if (operator === '-') {
-//   result = subtract(number1, number2);
-// } else if (operator === '*') {
-//   result = multiply(number1, number2);
-// } else if (operator === '/') {
-//   result = divide(number1, number2);
-// }
+
 
 // // Print the result to the console
 // console.log(result);
